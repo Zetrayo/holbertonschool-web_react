8315af6d08da834aae9d1cccad944c8383ac8dea
Zetrayo <josefeliciano.com98@gmail.com>
2025-01-31 17:48:10 +0100

    FIX : you wont believe this, more mistakes fixed

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,27 +1,27 @@
 export interface MajorCredits {
-    brand: string;
+    _brand: string;
     credits: number;
   }
 export interface MinorCredits {
-    brand: string;
+    _brand: string;
     credits: number;
   }
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
     return {
       credits: subject1.credits + subject2.credits,
-      brand: "MajorCredits"
+      _brand: "MajorCredits"
     };
   }
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
     return {
       credits: subject1.credits + subject2.credits,
-      brand: "MinorCredits"
+      _brand: "MinorCredits"
     };
   }
-const subject1: MajorCredits = { credits: 15, brand: 'MajorCredits' };
-const subject2: MajorCredits = { credits: 20, brand: 'MajorCredits' };
-const subject3: MinorCredits = { credits: 5, brand: 'MinorCredits' };
-const subject4: MinorCredits = { credits: 3, brand: 'MinorCredits' };
+const subject1: MajorCredits = { credits: 15, _brand: 'MajorCredits' };
+const subject2: MajorCredits = { credits: 20, _brand: 'MajorCredits' };
+const subject3: MinorCredits = { credits: 5, _brand: 'MinorCredits' };
+const subject4: MinorCredits = { credits: 3, _brand: 'MinorCredits' };
 const totalMajorCredits = sumMajorCredits(subject1, subject2);
 console.log('Total Major Credits:', totalMajorCredits);
 const totalMinorCredits = sumMinorCredits(subject3, subject4);
