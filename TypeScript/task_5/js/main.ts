7613138e50151aee58ae0b2ef135c6d96ea8dd61
Zetrayo <josefeliciano.com98@gmail.com>
2025-01-31 17:55:16 +0100

    FIX : mistake fix

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,27 +1,31 @@
 export interface MajorCredits {
-    _brand: string;
+    _brandMajor: void;
     credits: number;
-  }
+}
+
 export interface MinorCredits {
-    _brand: string;
+    _brandMinor: void;
     credits: number;
-  }
+}
+
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
     return {
-      credits: subject1.credits + subject2.credits,
-      _brand: "MajorCredits"
+        credits: subject1.credits + subject2.credits,
+        _brandMajor: undefined,
     };
-  }
+}
+
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
     return {
-      credits: subject1.credits + subject2.credits,
-      _brand: "MinorCredits"
+        credits: subject1.credits + subject2.credits,
+        _brandMinor: undefined,
     };
-  }
-const subject1: MajorCredits = { credits: 15, _brand: 'MajorCredits' };
-const subject2: MajorCredits = { credits: 20, _brand: 'MajorCredits' };
-const subject3: MinorCredits = { credits: 5, _brand: 'MinorCredits' };
-const subject4: MinorCredits = { credits: 3, _brand: 'MinorCredits' };
+}
+
+const subject1: MajorCredits = { credits: 15, _brandMajor: undefined };
+const subject2: MajorCredits = { credits: 20, _brandMajor: undefined };
+const subject3: MinorCredits = { credits: 5, _brandMinor: undefined };
+const subject4: MinorCredits = { credits: 3, _brandMinor: undefined };
 const totalMajorCredits = sumMajorCredits(subject1, subject2);
 console.log('Total Major Credits:', totalMajorCredits);
 const totalMinorCredits = sumMinorCredits(subject3, subject4);
